a70043c3a4a6a36bd5045b60a9c56fc088032e9e
paulyana <paulyana2000@gmail.com>
2024-11-29 12:41:06 -0300

    retorna rapa lista com id correto de filiado

--- View/lista-dependentes-view.php
+++ View/lista-dependentes-view.php
@@ -10,7 +10,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/../sindicatodosestagios/View/css/styles.css">
-    <title>Filiados</title>
+    <title>Dependentes</title>
 </head>
 <body>
 <main>
@@ -42,14 +42,15 @@
                         <?php if($bAparecerBotao): ?>
                             <td>
                                 <form action="http://localhost/sindicatodosestagios/dependente/editar" method="post">
-                                    <input type="hidden" name="id" value="<?= $dependente->getIId()  ?>">
+                                    <input type="hidden" name="dpe_id" value="<?= $dependente->getIId()  ?>">
+                                    <input type="hidden" name="flo_id" value="<?= $dependente->getIIdFiliadoAssociado()?>">
                                     <input type="submit" class="botao-editar" value="Editar">
                                 </form>
                             </td>
                             <td>
                                 <form action="http://localhost/sindicatodosestagios/dependente/deletar" method="post">
                                     <input type="hidden" name="dpe_id" value="<?= $dependente->getIId() ?>">
-                                    <input type="hidden" name="flo_id" value="<?= $dependente->getIIdFiliadoAssociado() ?>">
+                                    <input type="hidden" name="flo_id" value="<?= $dependente->getIIdFiliadoAssociado()?>">
                                     <input type="submit" class="botao-excluir" value="Excluir">
                                 </form>
                             </td>
@@ -66,11 +67,10 @@
     </section>
 
     <?php if($bAparecerBotao): ?>
-    <form action="http://localhost/sindicatodosestagios/dependente/cadastrar" method="post">
-        <input type="hidden" name="id_flo" value="<?= $iIdFiliadoAssociado?>">
-        <input type="submit" class="botao-cadastar-dependente" value="Cadastrar Novo Dependente">
-    </form>
+        <a class="botao-voltar-menu" href="http://localhost/sindicatodosestagios/dependente/cadastrar?flo_id=<?= $iIdFiliadoAssociado?>">Cadastrar Novo Dependente</a><br>
     <?php endif?>
+
+
     <a class="botao-voltar-menu" href="http://localhost/sindicatodosestagios/filiado/listar">Voltar</a>
     <a class="botao-sair" href="http://localhost/sindicatodosestagios/usuario/logout">Sair</a>
 
