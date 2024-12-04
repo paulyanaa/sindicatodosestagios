423153bb4d4f130aaded4553fd8e53bbcc8604d9
paulyana <paulyana2000@gmail.com>
2024-12-04 16:27:19 -0300

    adiciona variavel de ambiente na url

--- View/home-view.php
+++ View/home-view.php
@@ -8,13 +8,12 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/../sindicatodosestagios/View/css/styles.css">
-
     <title>Home</title>
 </head>
 <body>
 <main>
     <h1>Bem vindo ao Sindicato dos Estágios</h1>
-    <a class="botao-login" href='http://localhost/sindicatodosestagios/usuario/login'>Acessar Sistema</a>
+    <a class="botao-login" href="<?php echo Ambiente::getUrl('usuario/login')?>">Acessar Sistema</a>
     <footer>
         <p>&copy; 2024 Paulyana Ferreira. Todos os direitos reservados.</p>
     </footer>
