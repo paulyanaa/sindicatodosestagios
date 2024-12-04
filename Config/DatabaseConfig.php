a6561360a6ed389d131074bb3cd4eb61f7518c34
paulyana <paulyana2000@gmail.com>
2024-12-04 17:14:54 -0300

    configura os dados recebidos do .json

---
+++ Config/DatabaseConfig.php
@@ -0,0 +1,25 @@
+<?php
+
+
+class DatabaseConfig{
+
+    public static function getConeccao(){
+
+        $sArquivoJson = '/opt/lampp/htdocs/sindicatodosestagios/Config/dadosDB.json';
+
+        $jsonData = file_get_contents($sArquivoJson);
+
+
+        if (empty($jsonData)) {
+        die('Erro ao ler o arquivo JSON.');
+        }
+
+        $aDadosBD = json_decode($jsonData, true);
+
+        if ($aDadosBD === null) {
+            die('Erro ao decodificar o JSON.');
+        }
+
+        return $aDadosBD;
+    }
+}
