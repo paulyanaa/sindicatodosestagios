499742773e26738ba914cb076af4c4e839c5e380
paulyana <paulyana2000@gmail.com>
2024-12-10 12:50:21 -0300

    reorganiza o nome da classe recebida

--- autoload.php
+++ autoload.php
@@ -2,11 +2,12 @@
 
 spl_autoload_register(function ($sClasse) {
 
+    $sClasse = str_replace('\\', '/', $sClasse);
     $sArquivo = __DIR__ . "/Controller/$sClasse.php";
-    var_dump($sArquivo);
-    if(file_exists($sArquivo)){
+
+    if (file_exists($sArquivo)) {
         require_once $sArquivo;
     } else {
-        throw new Exception("Classe '$sClasse' não encontrada.");
+        throw new Exception("Classe '$sClasse' não encontrada. Caminho procurado: $sArquivo");
     }
 });
