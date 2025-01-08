0c242bccfe13136ea5180e792ae656013f8dc807
paulyana <paulyana2000@gmail.com>
2025-01-08 15:25:56 -0300

    documenta classe e métodos

--- src/Config/AmbienteConfig.php
+++ src/Config/AmbienteConfig.php
@@ -1,11 +1,23 @@
 <?php
-
-
 namespace Moobi\SindicatoDosEstagios\Config;
+
+/**
+ * Class AmbienteConfig
+ * @package Moobi\SindicatoDosEstagios\Config
+ * @version 1.0.0
+ */
 class AmbienteConfig
 {
-	public static function getUrl(string $sCaminho): string
-	{
+	/**
+	 * Cria variável de ambiente.
+	 *
+	 * @param string $sCaminho
+	 * @author Paulyana Ferreira paulyanasilva@moobitech.com.br
+	 * @return string
+	 *
+	 * @since 1.0.0
+	 */
+	public static function getUrl(string $sCaminho): string {
 		$sUrlBase = 'http://localhost:80/sindicatodosestagios/';
 		return $sUrlBase . $sCaminho;
 	}
