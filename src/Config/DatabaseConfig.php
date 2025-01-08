0fa1486e8ff18f2214bca4dd77c94f3c2d5410e7
paulyana <paulyana2000@gmail.com>
2025-01-08 15:28:50 -0300

    documenta classe e métodos

--- src/Config/DatabaseConfig.php
+++ src/Config/DatabaseConfig.php
@@ -1,10 +1,23 @@
 <?php
 
 namespace Moobi\SindicatoDosEstagios\Config;
+
+/**
+ * Class DatabaseConfig
+ * @package Moobi\SindicatoDosEstagios\Config
+ * @version 1.0.0
+ */
 class DatabaseConfig
 {
-	public static function getConexao()
-	{
+	/**
+	 * Transforma arquivo json com dados do Banco em array
+	 *
+	 * @author Paulyana Ferreira paulyanasilva@moobitech.com.br
+	 * @return mixed|void
+	 *
+	 * @since 1.0.0
+	 */
+	public static function getConexao() {
 		$sArquivoJson = __DIR__ . '/../Config/dadosDB.json';
 		$rJsonData = file_get_contents($sArquivoJson);
 
